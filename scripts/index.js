1bec83645bdef56310f1a92d95964772358ccf06
bjthe4 <bjthe4@gmail.com>
2023-12-27 14:37:53 +0100

    Aroudn the U.S. project 4 stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,9 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditButton.addEventListner("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
